38abe5085a15572d8b1a9e663f096c707c26a9d7
ArmykOliva <krystof.olik@gmail.com>
2022-01-02 12:56:33 +0100

    nice

---
+++ 9 ryby/target/debug/deps/ryby-a709f1da1bb4b3b1.d
@@ -0,0 +1,5 @@
+c:\Users\kryst\Desktop\prace\fiks2021\9 ryby\target\debug\deps\ryby-a709f1da1bb4b3b1.rmeta: src\main.rs
+
+c:\Users\kryst\Desktop\prace\fiks2021\9 ryby\target\debug\deps\ryby-a709f1da1bb4b3b1.d: src\main.rs
+
+src\main.rs:
---
+++ 9 ryby/target/debug/deps/ryby-bbbc3b72d1164968.d
@@ -0,0 +1,5 @@
+c:\Users\kryst\Desktop\prace\fiks2021\9 ryby\target\debug\deps\ryby-bbbc3b72d1164968.rmeta: src\main.rs
+
+c:\Users\kryst\Desktop\prace\fiks2021\9 ryby\target\debug\deps\ryby-bbbc3b72d1164968.d: src\main.rs
+
+src\main.rs:
